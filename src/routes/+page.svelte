d434f308f6bf76a79748e276ab73d161abe85c78
diytramboss <diytramboss@outlook.com>
2025-11-29 16:24:04 +0700

    Header + Footer On every Page!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import MovingTitle from "../MovingTitle.svelte";
+	import Header from "../Header.svelte"
 </script>
 
 <svelte:head>
-	<title bind:this=title>Home - STEAMachines</title>
+	<title>Home - STEAMachines</title>
 	<meta name="description" content="Welcome to STEAMachines, where innovation meets education. Explore our projects, team, and mission to inspire the next generation of creators." />
 </svelte:head>
 
-<MovingTitle />
+
 
 
 
