cb475e8fbc20c289762fbdb7ed62985d4bc7a4d4
diytramboss <diytramboss@outlook.com>
2025-12-15 06:57:56 +0700

    W

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,18 +8,18 @@
 <div class="flex flex-col">
     <div id="leadImage" class="flex w-full h-lvh static">
         <img src="/team.jpg" alt="Team" class="w-full h-full object-cover mask-b-from-85% mask-b-to-90% static">
-        <h1 class="absolute bg-b dark:bg-c m-4 rounded-lg p-1 text-6xl font-bold z-1 top-140">About Us</h1>
+        <h1 class="absolute bg-b dark:bg-c m-4 rounded-lg p-1 text-6xl font-bold z-1 top-190">About Us</h1>
     </div>
     <div id="VM" class="flex flex-row h-lvh max-w-4xl self-center mx-auto gap-4 my-10">
         <div id="Vision" class="flex flex-row w-1/2 gap-4 items-start xs:flex-col">
-            <img src="/vision-icon.png" alt="" class="w-32 h-32">
+            <img src="/vision-icon.png" alt="" class="w-32 h-32 dark:invert">
             <div class="flex flex-col gap-4">
                 <h1 class="text-4xl font-bold">Vision</h1>
                 <p class="text-wrap wrap-normal text-2xl">Become a competitive robotics team to conquer the challenges of the FTC competition, and play an active role as a driving force in introducing the STEAM system and robotics to the community.</p>
            </div>
         </div>
         <div id="Mission" class="flex flex-row w-1/2 gap-4 xs:flex-col">
-            <img src="/mission-icon.png" alt="" class="justify-center w-32 h-32">
+            <img src="/mission-icon.png" alt="" class="justify-center w-32 h-32 dark:invert">
             <div class="flex flex-col gap-4">
                 <h1 class="text-4xl font-bold">Mission</h1>
                 <p class="text-wrap wrap-normal text-2xl">1. Participate in FTC competitions.<br>
