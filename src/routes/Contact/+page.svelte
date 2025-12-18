b288489a8a7d48898b2893bd6bffac02d1683567
diytramboss <diytramboss@outlook.com>
2025-12-18 23:32:05 +0700

    Team History, Styleing

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -7,15 +7,15 @@
 
 <div class="top-lvh flex flex-col w-4xl self-center mx-auto gap-4 my-10">
     <div id="Info" class="flex flex-col text-2xl bg-b dark:bg-c rounded-lg justify bg-center gap-4 p-4">
-    	<div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 1 Raihan" class="w-12 h-12 dark:invert">Raihan<a target="_blank" href="https://wa.me/">+62 </a></div>
-        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert"><a target="_blank" href="https://wa.me/">+62 </a></div>
-        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert"><a target="_blank" href="https://whatsapp.com">Whatsapp Channel</a></div>
+    	<div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 1 Raihan" class="w-12 h-12 dark:invert">Raihan<a target="_blank" href="https://wa.me/628122732347">+62 812-2732-347</a></div>
+        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert">Aziz <a target="_blank" href="https://wa.me/6282211224215">+62 822-1122-4215</a></div>
+        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="WA Channel" class="w-12 h-12 dark:invert"><a target="_blank" href="https://whatsapp.com/channel/0029VbBO72NCHDysImsemT1C">Whatsapp Channel</a></div>
         <div class="flex flex-row gap-4"><img src="/instagram.png" alt="Instagram" class="w-12 h-12 dark:invert"><a target="_blank" href="https://instagram.com/steammachines42/">@steammachines42</a></div>
         <div class="flex flex-row gap-4"><img src="/twitter.png" alt="X/Twitter" class="w-12 h-12 dark:invert"><a target="_blank" href="https://x.com/FTCSteamMachine">@FTCSteamMachine</a></div>
         <div class="flex flex-row gap-4"><img src="/youtube.png" alt="Youtube" class="w-12 h-12 dark:invert"><a target="_blank" href="https://youtube.com/@ftcsteammachines15242">@ftcsteammachines15242</a></div>
         <div class="flex flex-row gap-4"><img src="/discord.png" alt="Discord" class="w-12 h-12 dark:invert"><a target="_blank" href=""></a></div>
         <div class="flex flex-row gap-4"><img src="/facebook.png" alt="Facebook" class="w-12 h-12 dark:invert"><a target="_blank" href="https://facebook.com/profile.php?id=61581189765153">Steam Machines</a></div>
-        <div class="flex flex-row gap-4"><img src="/email.png" alt="Email" class="w-12 h-12 dark:invert"><a target="_blank" href="mailto:">@gmail.com</a></div>
+        <div class="flex flex-row gap-4"><img src="/email.png" alt="Email" class="w-12 h-12 dark:invert"><a target="_blank" href="mailto:steamachines_42@Outlook.com">steamachines_42@Outlook.com</a></div>
     </div>
     <div id="Email Form">
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,11 +6,11 @@
 </svelte:head>
 
 <div class="flex flex-col">
-    <div id="leadImage" class="flex w-full h-lvh static">
-        <img src="/team.jpg" alt="Team" class="w-full h-full object-cover mask-b-from-85% mask-b-to-90% static">
-        <h1 class="absolute bg-b dark:bg-c m-4 rounded-lg p-1 text-6xl font-bold z-1 top-190">About Us</h1>
+    <div id="leadImage" class="flex flex-col w-full h-svh">
+        <img src="/team.jpg" alt="Team" class="w-full h-full object-cover mask-b-from-85% mask-b-to-90% z-0 static">
+        <h1 class="absolute bg-b dark:bg-c rounded-lg p-1 text-6xl font-bold z-10 bottom-5 left-10">About Us</h1>
     </div>
-    <div id="VM" class="flex flex-row h-lvh max-w-4xl self-center mx-auto gap-4 my-10">
+    <div id="VM" class="flex flex-row h-full lg:h-lvh max-w-4xl self-center mx-auto p-10 gap-4 my-10">
         <div id="Vision" class="flex flex-row w-1/2 gap-4 items-start xs:flex-col">
             <img src="/vision-icon.png" alt="" class="w-32 h-32 dark:invert">
             <div class="flex flex-col gap-4">
@@ -29,10 +29,13 @@
             </div>
         </div>
     </div>
-    <div id="TeamHistory" class="px-10">
-        <h1 class="text-4xl font-bold gap-4 mx-auto">Team History</h1>
-        <p>
-            
+    <div id="TeamHistory" class="flex flex-col gap-4 px-10 text-left">
+        <h1 class="text-4xl font-bold gap-4 mx-auto ">Team History</h1>
+        <p class="text-2xl">
+            STEAM MACHINES was founded in September 2021. Our team consists of all 11 male students and 1 team coach. STEAM MACHINES is the first team created by Techno Natura Jogja because previously all teams were created and came from Techno Natura Depok, our alliance is the team from Depok, the R2045 team.<br><br>
+STEAM MACHINE is a team created by the first generation at Techno Natura Jogja, even though we are the 3rd generation. We are the second generation of STEAM MACHINE. We are trying to continue the struggle for STEAM ENGINE, by following in the footsteps of his struggle. The first generation of STEAM MACHINE has trusted us to continue its struggle. The name STEAM MACHINES also has meaning because the name has two separate words, namely steam and machines. Steam comes from steam education, which means STEAM, Technology, Engineering, Art, Mathematics. As for machines, it means the machines that are in and control the robots that we make <br> <br>
+In our team, members also have various roles and tasks, including Engineer, Business Team, Media Team, 2D/3D Designer and Public Relationship. Engineers who are in charge of making robots in our team also have several interests consisting of Mechanics who focus on assembling robots, Electrical who focus on electrical performance on robots, Programmers who are in charge of programming robots. The role of the Business Team is to manage capital and finances for the team. Media is in charge of documenting, uploading team trips, for 2D/3D Designers are in charge of designing anything needed for the team such as Robots, Merchandise, banners and many more. In addition to making robots, our team also makes various merchandise, such as team pins, tumblers, jerseys, jackets and corps. And Public Relationship is in charge of introducing and interacting socially with the outside community such as Outreach, Events, cooperation with other companies and many more. Our goal is to participate in competitions from FTC and IFR to complete the challenges of the competition, and we also introduce the STEAM Education learning method and learn about robotics. <br> <br>
+
         </p>
     </div>
 </div>
