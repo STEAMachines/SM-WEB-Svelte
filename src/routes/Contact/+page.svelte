8445f9b59062b0ead0462b67a8620a3cbe527fb6
STEAMachines <STEAMachines_42@outlook.com>
2026-01-14 15:11:08 +0700

    Contatc width

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -13,48 +13,6 @@
 	<title>STEAMachines - Contact Us</title>
 </svelte:head>
 
-<div class="top-lvh flex flex-row w-4xl self-center mx-auto gap-4 my-10">
-    <!-- <div id="Info" class="flex flex-col text-2xl bg-b dark:bg-c rounded-lg justify bg-center gap-4 p-4">
-    	<div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 1 Raihan" class="w-12 h-12 dark:invert">Raihan<a target="_blank" href="https://wa.me/628122732347">+62 812-2732-347</a></div>
-        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert">Aziz <a target="_blank" href="https://wa.me/6282211224215">+62 822-1122-4215</a></div>
-        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="WA Channel" class="w-12 h-12 dark:invert"><a target="_blank" href="https://whatsapp.com/channel/0029VbBO72NCHDysImsemT1C">Whatsapp Channel</a></div>
-        <div class="flex flex-row gap-4"><img src="/instagram.png" alt="Instagram" class="w-12 h-12 dark:invert"><a target="_blank" href="https://instagram.com/steammachines42/">@steammachines42</a></div>
-        <div class="flex flex-row gap-4"><img src="/twitter.png" alt="X/Twitter" class="w-12 h-12 dark:invert"><a target="_blank" href="https://x.com/FTCSteamMachine">@FTCSteamMachine</a></div>
-        <div class="flex flex-row gap-4"><img src="/youtube.png" alt="Youtube" class="w-12 h-12 dark:invert"><a target="_blank" href="https://youtube.com/@ftcsteammachines15242">@ftcsteammachines15242</a></div>
-        <div class="flex flex-row gap-4"><img src="/discord.png" alt="Discord" class="w-12 h-12 dark:invert"><a target="_blank" href=""></a></div>
-        <div class="flex flex-row gap-4"><img src="/facebook.png" alt="Facebook" class="w-12 h-12 dark:invert"><a target="_blank" href="https://facebook.com/profile.php?id=61581189765153">Steam Machines</a></div>
-        <div class="flex flex-row gap-4"><img src="/email.png" alt="Email" class="w-12 h-12 dark:invert"><a target="_blank" href="mailto:steamachines_42@Outlook.com">steamachines_42@Outlook.com</a></div>
-    </div> -->
-    <div class="contact-container">
-        <section class="form-section">
-            <h2>DIRECT MESSAGES</h2>
-            
-            <form action="https://formspree.io/f/YOUR_FORM_ID" method="POST" on:submit={handleSubmit}>
-            <div class="input-group">
-                <label for="name">Full Name</label>
-                <input type="text" id="name" name="name" bind:value={name} placeholder="Enter your Name" required />
-            </div>
-
-            <div class="input-group">
-                <label for="email">Email Address</label>
-                <input type="email" id="email" name="_replyto" bind:value={email} placeholder="email@steamachines.com" required />
-            </div>
-
-            <div class="input-group">
-                <label for="message">Messages</label>
-                <textarea id="message" name="message" bind:value={message} rows="5" placeholder="Write your messages here...." required></textarea>
-            </div>
-
-            <button type="submit" class="submit-btn">Send Messages</button>
-            
-            {#if status}
-                <p class="status-msg">{status}</p>
-            {/if}
-            </form>
-        </section>
-    </div>
-</div>
-
 <style>
   .contact-container {
     margin: 40px auto;
@@ -151,3 +109,46 @@
 
   hr { border: 0; height: 1px; background: #eee; margin: 40px 0; }
 </style>
+
+<div class="flex flex-row w-4xl justify-center min-w-3/4 mx-auto px-20 gap-4 my-10">
+    <div id="Info" class="flex flex-col min-w-1/2 text-2xl bg-b dark:bg-c rounded-lg justify bg-center gap-4 p-4">
+    	<div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 1 Raihan" class="w-12 h-12 dark:invert">Raihan<a target="_blank" href="https://wa.me/628122732347">+62 812-2732-347</a></div>
+        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert">Aziz <a target="_blank" href="https://wa.me/6282211224215">+62 822-1122-4215</a></div>
+        <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="WA Channel" class="w-12 h-12 dark:invert"><a target="_blank" href="https://whatsapp.com/channel/0029VbBO72NCHDysImsemT1C">Whatsapp Channel</a></div>
+        <div class="flex flex-row gap-4"><img src="/instagram.png" alt="Instagram" class="w-12 h-12 dark:invert"><a target="_blank" href="https://instagram.com/steammachines42/">@steammachines42</a></div>
+        <div class="flex flex-row gap-4"><img src="/twitter.png" alt="X/Twitter" class="w-12 h-12 dark:invert"><a target="_blank" href="https://x.com/FTCSteamMachine">@FTCSteamMachine</a></div>
+        <div class="flex flex-row gap-4"><img src="/youtube.png" alt="Youtube" class="w-12 h-12 dark:invert"><a target="_blank" href="https://youtube.com/@ftcsteammachines15242">@ftcsteammachines15242</a></div>
+        <div class="flex flex-row gap-4"><img src="/discord.png" alt="Discord" class="w-12 h-12 dark:invert"><a target="_blank" href=""></a></div>
+        <div class="flex flex-row gap-4"><img src="/facebook.png" alt="Facebook" class="w-12 h-12 dark:invert"><a target="_blank" href="https://facebook.com/profile.php?id=61581189765153">Steam Machines</a></div>
+        <div class="flex flex-row gap-4"><img src="/email.png" alt="Email" class="w-12 h-12 dark:invert"><a target="_blank" href="mailto:steamachines_42@Outlook.com">steamachines_42@Outlook.com</a></div>
+    </div>
+    <div class="w-1/2 contact-container">
+        <section class="form-section">
+            <h2>DIRECT MESSAGES</h2>
+            
+            <form action="https://formspree.io/f/YOUR_FORM_ID" method="POST" on:submit={handleSubmit}>
+            <div class="input-group">
+                <label for="name">Full Name</label>
+                <input type="text" id="name" name="name" bind:value={name} placeholder="Cakra" required />
+            </div>
+
+            <div class="input-group">
+                <label for="email">Email Address</label>
+                <input type="email" id="email" name="_replyto" bind:value={email} placeholder="email@steamachines.com" required />
+            </div>
+
+            <div class="input-group">
+                <label for="message">Messages</label>
+                <textarea id="message" name="message" bind:value={message} rows="5" placeholder="I wuv SM! Recruit me!" required></textarea>
+            </div>
+
+            <button type="submit" class="dark:bg-c submit-btn">Send Messages</button>
+            
+            {#if status}
+                <p class="status-msg">{status}</p>
+            {/if}
+            </form>
+        </section>
+    </div>
+</div>
+
