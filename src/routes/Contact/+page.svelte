a9e7e0a4e6149d8ea631eaee6a9493ecbb2fd222
STEAMachines <steamachines_42@outlook.com>
2026-01-14 09:38:16 +0700

    fix contact-us #2

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -14,7 +14,7 @@
 </svelte:head>
 
 <div class="top-lvh flex flex-row w-4xl self-center mx-auto gap-4 my-10">
-    <div id="Info" class="flex flex-col text-2xl bg-b dark:bg-c rounded-lg justify bg-center gap-4 p-4">
+    <!-- <div id="Info" class="flex flex-col text-2xl bg-b dark:bg-c rounded-lg justify bg-center gap-4 p-4">
     	<div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 1 Raihan" class="w-12 h-12 dark:invert">Raihan<a target="_blank" href="https://wa.me/628122732347">+62 812-2732-347</a></div>
         <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="CP 2 " class="w-12 h-12 dark:invert">Aziz <a target="_blank" href="https://wa.me/6282211224215">+62 822-1122-4215</a></div>
         <div class="flex flex-row gap-4"><img src="/whatsapp.png" alt="WA Channel" class="w-12 h-12 dark:invert"><a target="_blank" href="https://whatsapp.com/channel/0029VbBO72NCHDysImsemT1C">Whatsapp Channel</a></div>
@@ -24,9 +24,7 @@
         <div class="flex flex-row gap-4"><img src="/discord.png" alt="Discord" class="w-12 h-12 dark:invert"><a target="_blank" href=""></a></div>
         <div class="flex flex-row gap-4"><img src="/facebook.png" alt="Facebook" class="w-12 h-12 dark:invert"><a target="_blank" href="https://facebook.com/profile.php?id=61581189765153">Steam Machines</a></div>
         <div class="flex flex-row gap-4"><img src="/email.png" alt="Email" class="w-12 h-12 dark:invert"><a target="_blank" href="mailto:steamachines_42@Outlook.com">steamachines_42@Outlook.com</a></div>
-
-
-    </div>
+    </div> -->
     <div class="contact-container">
         <section class="form-section">
             <h2>DIRECT MESSAGES</h2>
@@ -59,11 +57,11 @@
 
 <style>
   .contact-container {
-    max-width: 800px;
     margin: 40px auto;
     padding: 20px;
     font-family: 'Inter', sans-serif;
     color: #333;
+    width: 100%;
   }
 
   header {
